ab184ad376a90cfe56e8381a7b17dfc3b0432b82
urodriguez <ucielrodriguez1990@gmail.com>
2017-03-31 18:27:19 -0300

    Cargo primeras dependencias para el server

---
+++ server_app.js
@@ -0,0 +1,6 @@
+var express = require("express"),
+    app = express(),
+	path = require('path'),
+    bodyParser  = require("body-parser"),
+    methodOverride = require("method-override");
+    mongoose = require("mongoose");
